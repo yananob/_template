5aebc4067b6df361a97b8f676f3119cd1e5b9aa0
yananob <yananob@gmail.com>
2026-01-12 09:47:36 +0900

    update

--- tests/trigger_event_cloud_wishlist.sh
+++ tests/trigger_event_cloud_wishlist.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -eu
 
-gcloud pubsub topics publish oml-event --message='{"command": "batch-update-wishlist"}'
+gcloud pubsub topics publish XXX-event --message='{"command": "batch-update-wishlist"}'
